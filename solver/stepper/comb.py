6135ee4f59c64238af0c8ce4cecd38b22e68e61a
Kerry He <kerry.he@monashmotorsport.com>
2023-12-12 23:31:53 +1100

    Added centering if comb fails

--- solver/stepper/comb.py
+++ solver/stepper/comb.py
@@ -3,7 +3,7 @@ import math
 from utils.point import Point
 from utils import linear as lin
 
-alpha_sched = [0.999, 0.8, 0.7, 0.5, 0.3, 0.2, 0.1, 0.001, 0.000001]
+alpha_sched = [0.8, 0.7, 0.5, 0.3, 0.2, 0.1, 0.001]
 
 class CombinedStepper():
     def __init__(self, syssolver, model):
@@ -23,38 +23,45 @@ class CombinedStepper():
     def step(self, model, point, mu, verbose):
         self.syssolver.update_lhs(model)
 
-        if verbose:
-            print("  | %5s" % "comb", end="")
-
+        # Get prediction direction
         self.update_rhs_pred(model, point)
         self.dir_p.vec[:] = self.syssolver.solve_system(self.rhs, model, mu / point.tau / point.tau).vec
         self.res.vec[:] = self.rhs.vec - self.syssolver.apply_system(self.dir_p, model, mu / point.tau / point.tau).vec
         res_norm = np.linalg.norm(self.res.vec)
 
+        # Get TOA prediction direction
         self.update_rhs_pred_toa(model, point, mu, self.dir_p)
         self.dir_p_toa.vec[:] = self.syssolver.solve_system(self.rhs, model, mu / point.tau / point.tau).vec
         self.res.vec[:] = self.rhs.vec - self.syssolver.apply_system(self.dir_p_toa, model, mu / point.tau / point.tau).vec
         res_norm = max(np.linalg.norm(self.res.vec), res_norm)
 
+        # Get centering direction
         self.update_rhs_cent(model, point, mu)
         self.dir_c.vec[:] = self.syssolver.solve_system(self.rhs, model, mu / point.tau / point.tau).vec
         self.res.vec[:] = self.rhs.vec - self.syssolver.apply_system(self.dir_c, model, mu / point.tau / point.tau).vec
         res_norm = max(np.linalg.norm(self.res.vec), res_norm)
 
+        # Get TOA centering direction
         self.update_rhs_cent_toa(model, point, mu, self.dir_c)
         self.dir_c_toa.vec[:] = self.syssolver.solve_system(self.rhs, model, mu / point.tau / point.tau).vec
         self.res.vec[:] = self.rhs.vec - self.syssolver.apply_system(self.dir_c_toa, model, mu / point.tau / point.tau).vec
         res_norm = max(np.linalg.norm(self.res.vec), res_norm)
 
-        print(" %10.3e" % (res_norm), end="")
+        point, alpha, success = self.line_search_comb(model, point)
+        if not success:
+            point, alpha = self.line_search_cent(model, point)
+        
+        if verbose:
+            if success:
+                print("  | %5s" % "comb", "%10.3e" % (res_norm), " %5.3f" % (alpha))
+            else:
+                print("  | %5s" % "cent", "%10.3e" % (res_norm), " %5.3f" % (alpha))
 
-        point = self.line_search(model, point, verbose)
         
         return point
     
-    def line_search(self, model, point, verbose):
+    def line_search_comb(self, model, point):
         alpha_iter = 1
-        beta = 0.9
         eta = 0.99
 
         dir_p = self.dir_p
@@ -64,6 +71,9 @@ class CombinedStepper():
         next_point = Point(model)
 
         while True:
+            if alpha_iter >= len(alpha_sched):
+                return point, alpha, False
+
             alpha = alpha_sched[alpha_iter]
             # Step point in direction and step size
             next_point.vec[:] = point.vec + alpha * (dir_p.vec + alpha * dir_p_toa.vec) + (1.0 - alpha) * (dir_c.vec + (1.0 - alpha) * dir_c_toa.vec)
@@ -95,13 +105,55 @@ class CombinedStepper():
             # If feasible, return point
             if in_prox:
                 point.vec[:] = next_point.vec[:]
-
-                if verbose:
-                    print("  %5.3f" % (alpha))
-                return point
+                return point, alpha, True
         
             # Otherwise backtrack
             alpha_iter += 1
+
+    def line_search_cent(self, model, point):
+        alpha_iter = 1
+        eta = 0.99
+
+        dir_c = self.dir_c
+        dir_c_toa = self.dir_c_toa
+        next_point = Point(model)
+
+        while True:
+            alpha = alpha_sched[alpha_iter]
+            # Step point in direction and step size
+            next_point.vec[:] = point.vec + alpha * (dir_c.vec + alpha * dir_c_toa.vec)
+            mu = lin.inp(next_point.s, next_point.z) / model.nu
+            if mu < 0:
+                alpha_iter += 1
+                continue
+
+            rtmu = math.sqrt(mu)
+            irtmu = np.reciprocal(rtmu)
+            self.prox = 0.
+
+            # Check feasibility
+            in_prox = False
+            for (k, cone_k) in enumerate(model.cones):
+                cone_k.set_point(next_point.s_views[k] * irtmu)
+
+                in_prox = False
+                if cone_k.get_feas():
+                    grad_k = cone_k.get_grad()
+                    psi = next_point.z_views[k] * irtmu + grad_k
+                    prod = cone_k.invhess_prod(psi)
+                    self.prox = max(self.prox, lin.inp(prod, psi))
+
+                    in_prox = (self.prox < eta)
+                if not in_prox:
+                    break
+        
+            # If feasible, return point
+            if in_prox:
+                point.vec[:] = next_point.vec[:]
+                return point, alpha
+        
+            # Otherwise backtrack
+            alpha_iter += 1            
 
     def update_rhs_cent(self, model, point, mu):
         self.rhs.x.fill(0.)
