ed5f8e056f24e426329b2751bd99c3e203ca0dc5
kerry-he <69945252+kerry-he@users.noreply.github.com>
2024-07-20 21:28:34 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name="qics",
+    version="0.0",
+    author="Kerry He",
+    author_email="he.kerry.k@gmail.com",
+    description="Conic solver for quantum information theory",
+    packages=["qics"]
+)
