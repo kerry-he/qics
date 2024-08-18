5a3f1eb1d6541a3b83de24268356231b2ba94b9d
kerry-he <69945252+kerry-he@users.noreply.github.com>
2024-08-18 23:26:25 +1000

    Changed examples

--- examples/benchmark/generate/ea_channel_capacity_qre.py
+++ examples/benchmark/generate/ea_channel_capacity_qre.py
@@ -11,51 +11,48 @@ import qics.quantum as qu
 
 np.random.seed(1)
 
-n = 2
-N = n*n
+for n in range(2, 17):
+    N = n*n
 
-vn = vec.vec_dim(n, compact=True, iscomplex=True)
-vN = vec.vec_dim(N, compact=True, iscomplex=True)
+    vn = vec.vec_dim(n, compact=True, iscomplex=True)
+    vN = vec.vec_dim(N, compact=True, iscomplex=True)
 
-V = qu.random.stinespring_operator(n, n, n, iscomplex=True)
+    V = qu.random.stinespring_operator(n, n, n, iscomplex=True)
 
-# Define objective functions
-# with variables (X, (t, Y))
-c = np.vstack((np.array([[1.], [1.]]), np.zeros((vn, 1))))
+    # Define objective functions
+    # with variables (X, (t, Y))
+    c = np.vstack((np.array([[1.], [1.]]), np.zeros((vn, 1))))
 
-# Build linear constraints
-VV = vec.lin_to_mat(
-    lambda X : V @ X @ V.conj().T, (n, N), compact=(True, False), iscomplex=True
-)
-ikr_trB = vec.lin_to_mat(lambda X : qu.i_kr(qu.p_tr(X, (n, n), 1), (n, n), 1), (N, N), compact=(False, False), iscomplex=True)
-trE = vec.lin_to_mat(lambda X : qu.p_tr(X, (n, n), 0), (N, n), compact=(False, False), iscomplex=True)
+    # Build linear constraints
+    VV = vec.lin_to_mat(
+        lambda X : V @ X @ V.conj().T, (n, N), compact=(True, False), iscomplex=True
+    )
+    ikr_trB = vec.lin_to_mat(lambda X : qu.i_kr(qu.p_tr(X, (n, n), 1), (n, n), 1), (N, N), compact=(False, False), iscomplex=True)
+    trE = vec.lin_to_mat(lambda X : qu.p_tr(X, (n, n), 0), (N, n), compact=(False, False), iscomplex=True)
 
-# tr[X] = 1
-A = np.hstack((np.array([[0., 0.]]), vec.mat_to_vec(np.eye(n, dtype=np.complex128), compact=True).T))
-b = np.array([[1.]])
+    # tr[X] = 1
+    A = np.hstack((np.array([[0., 0.]]), vec.mat_to_vec(np.eye(n, dtype=np.complex128), compact=True).T))
+    b = np.array([[1.]])
 
-G1 = np.hstack((np.array([[1., 0.]]), np.zeros((1, vn))))
-G2 = np.hstack((np.zeros((2*N*N, 2)), VV))
-G3 = np.hstack((np.zeros((2*N*N, 2)), ikr_trB @ VV))
-G4 = np.hstack((np.array([[0., 1.]]), np.zeros((1, vn))))
-G5 = np.hstack((np.array([[0., 0.]]), np.zeros((1, vn))))
-G6 = np.hstack((np.zeros((2*n*n, 2)), trE @ VV))
-G7 = np.hstack((np.zeros((2*n*n, 2)), vec.eye(n, iscomplex=True).T))
-G = -np.vstack((G1, G2, G3, G4, G5, G6, G7))
+    G1 = np.hstack((np.array([[1., 0.]]), np.zeros((1, vn))))
+    G2 = np.hstack((np.zeros((2*N*N, 2)), VV))
+    G3 = np.hstack((np.zeros((2*N*N, 2)), ikr_trB @ VV))
+    G4 = np.hstack((np.array([[0., 1.]]), np.zeros((1, vn))))
+    G5 = np.hstack((np.array([[0., 0.]]), np.zeros((1, vn))))
+    G6 = np.hstack((np.zeros((2*n*n, 2)), trE @ VV))
+    G7 = np.hstack((np.zeros((2*n*n, 2)), vec.eye(n, iscomplex=True).T))
+    G = -np.vstack((G1, G2, G3, G4, G5, G6, G7))
 
-h = np.zeros((G.shape[0], 1))
-h[1 + 2*2*N*N + 1] = 1.
+    h = np.zeros((G.shape[0], 1))
+    h[1 + 2*2*N*N + 1] = 1.
 
-# Input into model and solve
-cones = [
-    qics.cones.QuantRelEntr(N, iscomplex=True),
-    qics.cones.QuantEntr(n, iscomplex=True),
-    qics.cones.PosSemidefinite(n, iscomplex=True)
-]
+    # Input into model and solve
+    cones = [
+        qics.cones.QuantRelEntr(N, iscomplex=True),
+        qics.cones.QuantEntr(n, iscomplex=True),
+        qics.cones.PosSemidefinite(n, iscomplex=True)
+    ]
 
-# Initialize model and solver objects
-model  = qics.Model(c=c, A=A, b=b, G=G, h=h, cones=cones)
-solver = qics.Solver(model)
-
-# Solve problem
-info = solver.solve()
+    # Initialize model and solver objects
+    model  = qics.Model(c=c, A=A, b=b, G=G, h=h, cones=cones)
+    qics.io.write_cbf(model, "ccea_qre_" + str(n) + ".cbf")
--- examples/benchmark/generate/qq_channel_capacity_qre.py
+++ examples/benchmark/generate/qq_channel_capacity_qre.py
@@ -11,44 +11,41 @@ import qics.quantum as qu
 
 np.random.seed(1)
 
-n = 2
-N = n*n
+for n in range(2, 17):
+    N = n*n
 
-vn = vec.vec_dim(n, compact=True, iscomplex=True)
-vN = vec.vec_dim(N, compact=True, iscomplex=True)
+    vn = vec.vec_dim(n, compact=True, iscomplex=True)
+    vN = vec.vec_dim(N, compact=True, iscomplex=True)
 
-V, W = qu.random.degradable_channel(n, n, n, iscomplex=True)
+    V, W = qu.random.degradable_channel(n, n, n, iscomplex=True)
 
-# Define objective functions
-# with variables (X, (t, Y))
-c = np.vstack((np.array([[1.]]), np.zeros((vn, 1))))
+    # Define objective functions
+    # with variables (X, (t, Y))
+    c = np.vstack((np.array([[1.]]), np.zeros((vn, 1))))
 
-# Build linear constraints
-WNW = vec.lin_to_mat(
-    lambda X : W @ qu.p_tr(V @ X @ V.conj().T, (n, n), 1) @ W.conj().T, (n, N), compact=(True, False), iscomplex=True
-)
-ikr_trB = vec.lin_to_mat(lambda X : qu.i_kr(qu.p_tr(X, (n, n), 1), (n, n), 1), (N, N), compact=(False, False), iscomplex=True)
-# tr[X] = 1
-A = np.hstack((np.array([[0.]]), vec.mat_to_vec(np.eye(n, dtype=np.complex128), compact=True).T))
-b = np.array([[1.]])
+    # Build linear constraints
+    WNW = vec.lin_to_mat(
+        lambda X : W @ qu.p_tr(V @ X @ V.conj().T, (n, n), 1) @ W.conj().T, (n, N), compact=(True, False), iscomplex=True
+    )
+    ikr_trB = vec.lin_to_mat(lambda X : qu.i_kr(qu.p_tr(X, (n, n), 1), (n, n), 1), (N, N), compact=(False, False), iscomplex=True)
+    # tr[X] = 1
+    A = np.hstack((np.array([[0.]]), vec.mat_to_vec(np.eye(n, dtype=np.complex128), compact=True).T))
+    b = np.array([[1.]])
 
-G1 = np.hstack((np.array([[1.]]), np.zeros((1, vn))))
-G2 = np.hstack((np.zeros((2*N*N, 1)), WNW))
-G3 = np.hstack((np.zeros((2*N*N, 1)), ikr_trB @ WNW))
-G4 = np.hstack((np.zeros((2*n*n, 1)), vec.eye(n, iscomplex=True).T))
-G = -np.vstack((G1, G2, G3, G4))
+    G1 = np.hstack((np.array([[1.]]), np.zeros((1, vn))))
+    G2 = np.hstack((np.zeros((2*N*N, 1)), WNW))
+    G3 = np.hstack((np.zeros((2*N*N, 1)), ikr_trB @ WNW))
+    G4 = np.hstack((np.zeros((2*n*n, 1)), vec.eye(n, iscomplex=True).T))
+    G = -np.vstack((G1, G2, G3, G4))
 
-h = np.zeros((G.shape[0], 1))
+    h = np.zeros((G.shape[0], 1))
 
-# Input into model and solve
-cones = [
-    qics.cones.QuantRelEntr(N, iscomplex=True),
-    qics.cones.PosSemidefinite(n, iscomplex=True)
-]
+    # Input into model and solve
+    cones = [
+        qics.cones.QuantRelEntr(N, iscomplex=True),
+        qics.cones.PosSemidefinite(n, iscomplex=True)
+    ]
 
-# Initialize model and solver objects
-model  = qics.Model(c=c, A=A, b=b, G=G, h=h, cones=cones)
-solver = qics.Solver(model)
-
-# Solve problem
-info = solver.solve()
+    # Initialize model and solver objects
+    model  = qics.Model(c=c, A=A, b=b, G=G, h=h, cones=cones)
+    qics.io.write_cbf(model, "qqcc_qre_" + str(n) + ".cbf")
