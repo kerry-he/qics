cf96caeeac18a087527fb55efb5386b59316f58d
kerry-he <he.kerry.k@gmail.com>
2024-09-06 10:03:56 +1000

    Updated ikr and ptr tests

--- tests/quantum.py
+++ tests/quantum.py
@@ -12,7 +12,15 @@ def test_ptr_ikr():
     X = np.random.randn(x_dim, x_dim) + np.random.randn(x_dim, x_dim)*1j
     X = X + X.conj().T
 
-    for (sys, dim) in enumerate(dims):
+    sys_tests = [
+        0, 1, 2, 3, 
+        (0, 1), (2, 0), (0, 3), (2, 1), (1, 3), (3, 2),
+        (0, 1, 2), (3, 0, 1), (0, 3, 2), (3, 2, 1),
+        (0, 1, 2, 3)
+    ]
+
+    for sys in sys_tests:
+        dim = dims[sys] if isinstance(sys, int) else np.prod([dims[k] for k in sys])
         y_dim = x_dim // dim
         Y = np.random.randn(y_dim, y_dim) + np.random.randn(y_dim, y_dim)*1j
         Y = Y + Y.conj().T
@@ -36,15 +44,24 @@ def test_ptr_ikr_multi():
     Xs = np.random.randn(5, x_dim, x_dim) + np.random.randn(5, x_dim, x_dim)*1j
     Xs = Xs + Xs.conj().transpose(0, 2, 1)
 
-    for (sys, dim) in enumerate(dims):
+    sys_tests = [
+        0, 1, 2, 3, 
+        (0, 1), (2, 0), (0, 3), (2, 1), (1, 3), (3, 2),
+        (0, 1, 2), (3, 0, 1), (0, 3, 2), (3, 2, 1),
+        (0, 1, 2, 3)
+    ]
+
+    for sys in sys_tests:
+        dim = dims[sys] if isinstance(sys, int) else np.prod([dims[k] for k in sys])
         y_dim = x_dim // dim
         Ys = np.random.randn(5, y_dim, y_dim) + np.random.randn(5, y_dim, y_dim)*1j
         Ys = Ys + Ys.conj().transpose(0, 2, 1)
 
         assert np.allclose(
-            np.trace(p_tr_multi(np.zeros_like(Ys), Xs, dims, sys) @ Ys, axis1=1, axis2=2), 
-            np.trace(Xs @ i_kr_multi(np.zeros_like(Xs), Ys, dims, sys), axis1=1, axis2=2)
+            np.trace(p_tr_multi(np.ones_like(Ys), Xs, dims, sys) @ Ys, axis1=1, axis2=2), 
+            np.trace(Xs @ i_kr_multi(np.ones_like(Xs), Ys, dims, sys), axis1=1, axis2=2)
         ), "qics.quantum.p_tr_multi and qics.quantum.i_kr_multi are not adjoint linear operators"
 
 if __name__ == "__main__":
     test_ptr_ikr()
+    test_ptr_ikr_multi()
