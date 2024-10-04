9666e2c7e54cc3a95dc8c0f5df45ac6248a180fa
kerry-he <he.kerry.k@gmail.com>
2024-10-04 22:07:47 +1000

    Added copyright notices to files

--- qics/_stepper/sym.py
+++ qics/_stepper/sym.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2024, Kerry He, James Saunderson, and Hamza Fawzi
+
+# This Python package QICS is licensed under the MIT license; see LICENSE.md 
+# file in the root directory or at https://github.com/kerry-he/qics
+
 from qics.point import Point, VecProduct
 
 
--- qics/cones/possemidefinite.py
+++ qics/cones/possemidefinite.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2024, Kerry He, James Saunderson, and Hamza Fawzi
+
+# This Python package QICS is licensed under the MIT license; see LICENSE.md 
+# file in the root directory or at https://github.com/kerry-he/qics
+
 import numpy as np
 import scipy as sp
 import numba as nb
