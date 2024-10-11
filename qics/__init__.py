5ad3eb26013e5e65f12544240f4b5872daf5a459
kerry-he <he.kerry.k@gmail.com>
2024-10-11 12:50:12 +1100

    Fixed imports

--- qics/__init__.py
+++ qics/__init__.py
@@ -6,6 +6,7 @@
 # __init__.py
 from qics._version import __version__  # noqa
 
-import qics.io  # noqa
 from qics.solver import Solver  # noqa
 from qics.model import Model  # noqa
+
+import qics.io  # noqa
--- qics/quantum/__init__.py
+++ qics/quantum/__init__.py
@@ -4,9 +4,10 @@
 # file in the root directory or at https://github.com/kerry-he/qics
 
 # __init__.py
-import qics.quantum.random  # noqa
 
 from qics.quantum.entropy import entropy, purify  # noqa
 from qics.quantum.operator import p_tr, p_tr_multi  # noqa
 from qics.quantum.operator import i_kr, i_kr_multi  # noqa
 from qics.quantum.operator import partial_transpose, swap  # noqa
+
+import qics.quantum.random  # noqa
